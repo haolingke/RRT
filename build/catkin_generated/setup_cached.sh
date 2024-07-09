594a470daa7458b0531db68190168f1117b8050c
lilinghao23m <23s153021@stu.hit.edu.cn>
2024-07-09 16:16:34 +0800

    现在可以运行了，半小时前的提交launch文件未修改完毕

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -9,7 +9,6 @@
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/lilinghao/test/motion_plan_ws/src/RRT/build/devel:$CMAKE_PREFIX_PATH"
 export LD_LIBRARY_PATH="/home/lilinghao/test/motion_plan_ws/src/RRT/build/devel/lib:$LD_LIBRARY_PATH"
-export PATH='/opt/ros/noetic/bin:/opt/gcc-arm-none-eabi-9-2020-q2-update/bin:/home/lilinghao/.local/bin:/home/lilinghao/miniconda3/condabin:/opt/gcc-arm-none-eabi-9-2020-q2-update/bin:/home/lilinghao/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
 export PKG_CONFIG_PATH="/home/lilinghao/test/motion_plan_ws/src/RRT/build/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/lilinghao/test/motion_plan_ws/src/RRT/build'
 export ROS_PACKAGE_PATH="/home/lilinghao/test/motion_plan_ws/src/RRT:$ROS_PACKAGE_PATH"
